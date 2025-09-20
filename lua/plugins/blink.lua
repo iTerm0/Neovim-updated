98706fa6f52fe454ef6cfd3f085c574c77e8b619
parsa norozi <parsa@parsas-MacBook-Air.local>
2025-09-21 01:20:21 +0200

    inital commit

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,48 @@
+return {
+	"saghen/blink.cmp",
+	commit = "cb5e346d9e0efa7a3eee7fd4da0b690c48d2a98e",
+	opts = {
+		sources = {
+			default = { "lazydev" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
+		},
+		snippets = {
+			expand = function(snippet, _)
+				return LazyVim.cmp.expand(snippet)
+			end,
+		},
+		appearance = {
+			use_nvim_cmp_as_default = false,
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			accept = {
+				auto_brackets = {
+					enabled = true,
+				},
+			},
+			menu = {
+				draw = {
+					treesitter = { "lsp" },
+				},
+			},
+			documentation = {
+				auto_show = false,
+				auto_show_delay_ms = 200,
+			},
+			ghost_text = {
+				enabled = false,
+			},
+		},
+		keymap = {
+			preset = "super-tab",
+			["<C-y>"] = { "select_and_accept" },
+		},
+	},
+}
---
+++ lua/plugins/start.lua
@@ -0,0 +1,16 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				header = [[
+<leader><leader> :telescope
+ ]],
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+				},
+			},
+		},
+	},
+}
